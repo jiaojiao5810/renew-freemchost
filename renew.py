f51371bad2bdd31698f1f42ea7de00b37e9b46d8
jiaojiao5810 <jiaojiao5810@gmail.com>
2026-06-11 23:41:15 +0800

    1

--- renew.py
+++ renew.py
@@ -19,7 +19,7 @@ RENEW_ACTION_URL = "https://new.freemchost.com/_serverFn/798181797bd95a02dee916a
 # 【接口 B】获取最终完整状态的 Detail 路由
 RENEW_DETAIL_URL = "https://new.freemchost.com/_serverFn/c3a45c08362f2f613bbb6d511a3733a9e85e561709d48bec9280e82a4aa4f47d"
 
-SERVER_ID = "c1487010-5680-43b7-932b-f6b6de2d893c"
+SERVER_ID = "82f18f00-8d41-49e0-9b33-a4c8c6dd1faa"
 
 # 4. 消息推送配置（可选，可从 GitHub Secrets 读取，不需要保持 None）
 SCKEY = os.getenv("SCKEY")
